90389e73d1eebef857b6e13e3893e2b883282495
Toni <tonitamburkovski@gmail.com>
2020-03-21 03:28:08 +0100

    solved

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,6 +1,19 @@
 require "raindrops/version"
 
 module Raindrops
-  class Error < StandardError; end
-  # Your code goes here...
+  class Drop
+    def initialize(int)
+      @int = int
+      @get_factor = ["Pling", "Plang", "Plong"]
+    end
+
+    def factor
+      factor1 = ""
+      factor1 = @get_factor [0] if (@int%3).zero?
+      factor1 += @get_factor [1] if (@int%5).zero?
+      factor1 += @get_factor [2] if (@int%7).zero?
+
+      factor1.empty? ? @int.to_s : factor1
+    end
+  end
 end
--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -1,9 +1,47 @@
 RSpec.describe Raindrops do
+
   it "has a version number" do
     expect(Raindrops::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "if devided by 3 is Pling" do
+    rain_drop = Raindrops::Drop.new(3)
+    expect(rain_drop.factor).to eq('Pling')
   end
+
+  it "if devided by 5 is Plang" do
+    rain_drop = Raindrops::Drop.new(5)
+    expect(rain_drop.factor).to eq('Plang')
+  end
+
+  it "if devided by 7 is Plong" do
+    rain_drop = Raindrops::Drop.new(7)
+    expect(rain_drop.factor).to eq('Plong')
+  end
+
+  it "if devided by 3 & 5 is PlingPlang" do
+    rain_drop = Raindrops::Drop.new(15)
+    expect(rain_drop.factor).to eq('PlingPlang')
+  end
+
+  it "if devided by 3 & 7 is PlingPlong" do
+    rain_drop = Raindrops::Drop.new(21)
+    expect(rain_drop.factor).to eq('PlingPlong')
+  end
+
+  it "if devided by 5 & 7 is PlangPlong" do
+    rain_drop = Raindrops::Drop.new(35)
+    expect(rain_drop.factor).to eq('PlangPlong')
+  end
+
+  it "if devided by 3, 5 & 7 is PlingPlangPlong" do
+    rain_drop = Raindrops::Drop.new(105)
+    expect(rain_drop.factor).to eq('PlingPlangPlong')
+  end
+
+  it "if not devided by 3, 5 or 7 return the digit" do
+    rain_drop = Raindrops::Drop.new(8)
+    expect(rain_drop.factor).to eq('8')
+  end
+
 end
